960d076635b112bc73755d27a0b42c184dbf1ddd
jaysharmagithub <125895781+jaysharmagithub@users.noreply.github.com>
2024-04-06 00:49:57 +0530

    Update PrimeNumChecker1ofTheBestOptimizedWay.java

--- PrimeNumChecker1ofTheBestOptimizedWay.java
+++ PrimeNumChecker1ofTheBestOptimizedWay.java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+public class PrimeNumChecker1ofTheBestOptimized {
+  public static boolean isPrimeNum(int num) {
+    if(num<2) return false;
+    if(num % 2 == 0 || num % 3 == 0 ) return false;
+    if(num ==2 || num ==3) return true;
+    for(int k=5; k<=Math.sqrt(num); k+=6) {
+      if(num % (k+1) == 0 && num % (k+5) == 0) return false; 
+    }
+    return true;
+  }
+  
+  public static void main(String[] args) {
+    if(isPrimeNum(99991)) System.out.println("Prime");
+    else System.out.println("Not a Prime");
+  }
+}
