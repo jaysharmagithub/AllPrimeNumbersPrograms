cd871f1c3544dcc0f9a68d04e7f614c563ddb720
jaysharmagithub <125895781+jaysharmagithub@users.noreply.github.com>
2024-04-06 00:18:50 +0530

    completed PrimeNumCheckOptimizedWay.java

--- PrimeNumCheckOptimizedWay.java
+++ PrimeNumCheckOptimizedWay.java
@@ -1,1 +1,21 @@
+import java.util.*;
 
+public class PrimeNumCheckerOptimizedWay { 
+ 
+  //function definiton
+  public static boolean isPrimeNum(int num){
+    if(num<2) return false;
+    for(int i=2; i <= Math.sqrt(num); i++) {
+      if(num%i == 0) return false;
+        
+    }
+    return true;
+  }
+  public static void main(String[] args){
+  if( isPrimeNum(11) )
+            System.out.println(" Prime Number"); 
+        else
+            System.out.println(" not a Prime Number"); 
+       
+ }
+}
