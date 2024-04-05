0d6225d1597b5dc85bd622cd1894ba3162340415
jaysharmagithub <125895781+jaysharmagithub@users.noreply.github.com>
2024-04-05 23:11:30 +0530

    Update and Complete PrimeNumberChecker.java

--- PrimeNumberChecker.java
+++ PrimeNumberChecker.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+public class PrimeNumberChecker{
+  public static boolean isPrimeNumber(int n) {
+   if(n<2) return false;
+   if(n>2) {
+    for(int i=2; i<n; i++){
+      if(n%i != 0 ) return true;
+      break;
+    }
+    return false;  
+   }
+  }
+  
+  public static void main(String args[]){
+   int primeNum = 5;
+    if(isPrimeNumber(primeNum) System.out.println("prime number");
+    else System.out.println("not a prime number");
+  }
+}
